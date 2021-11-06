f22cb7189e3a5549ee78f908fd81d93da170a91c
Aaron Lu <aaronnoh0409@gmail.com>
2021-11-06 14:02:08 -0400

    back to the bad loop :(

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ function looping() {
                 npc.walkEast(1500, () => {
                     npc.walkSouth(1500, () => {
                         npc.walkWest(2700, () => {
-                            npc.walkNorth(400, () => {})
+                            npc.walkNorth(400, () => { 
+                                for (let i = 0; i < 1; i++) {
+                                    looping()
+                                }
+                            })
                         })
                     })
                 })
@@ -18,8 +22,7 @@ function looping() {
         })
     })
 }
-
-setInterval(looping)
+looping()
 
 // Create the inventory
 const inventory = newInventory()
